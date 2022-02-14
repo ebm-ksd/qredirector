f02064d8c6e6cf81389eecf9def438b6d7cf4ef7
Enmanuel Basulto Martínez <enmanuel.basulto@uic.cu>
2022-02-14 17:01:03 -0500

    Commit inicial

---
+++ inc/HTTPCodes.php
@@ -0,0 +1,18 @@
+<?php
+
+final class HTTPCodes {  
+    public const OK = "200 Ok";
+    public const CREATED = "201 Created";
+    public const ACCEPTED = "202 Accepted";
+    public const NO_CONTENT = "204 No Content";
+
+    public const MOVED_PERMANENTLY = "301 Moved Permanently";
+    public const FOUND = "302 Found";
+
+    public const BAD_REQUEST = "400 Bad Request";
+    public const UNAUTHORIZED = "401 Unauthorized";
+    public const FORBIDDEN = "403 Forbidden";
+    public const NOT_FOUND = "404 Not Found";
+    
+    public const INTERNAL_SERVER_ERROR = "500 Internal Server Error";
+}
---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+
+function addslashesRecursivo($var) {
+    if (!is_array($var)) {
+        return addslashes(htmlentities($var));
+    }
+        
+    $new_var = array();
+    foreach ($var as $k => $v)
+        $new_var[addslashes(htmlentities($k))] = addslashesRecursivo($v);
+        
+    return $new_var;
+}
+
+$_GET = addslashesRecursivo($_GET);
+$_SERVER = addslashesRecursivo($_SERVER);
+
+final class index {
+    private $Response = null;
+
+    public function exception_handler($ex): void {
+        if ($ex instanceof NotFoundException) {
+            $this->Response->StatusCode = HTTPCodes::NOT_FOUND;
+        } else {
+            $this->Response->StatusCode = HTTPCodes::INTERNAL_SERVER_ERROR;
+        }
+        $this->Response->Body = array("error" => $ex->getMessage());
+        exit($this->Response->send());
+    }
+    
+    
+    public function autoload_function($name): void {
+        $class = "./inc/".$name.".php";
+
+        if(!file_exists($class))
+        {
+            throw new NotFoundException("Class '".$name."' not found.", 1);
+        }
+        
+        include_once $class;
+    }
+
+    public function init(): void {
+        error_reporting(E_ALL | E_STRICT);
+        set_exception_handler(array($this, 'exception_handler'));
+        spl_autoload_register(array($this, 'autoload_function'));
+        
+        $request = HTTPRequest::fromGlobals();
+        $this->Response = new HTTPResponse($request->Protocol);
+
+        $this->Response->ContentType = "application/json; charset=UTF-8";
+        $this->Response->addHeader("Access-Control-Allow-Origin", "*");
+        $this->Response->addHeader("Access-Control-Allow-Methods", "GET");
+        $this->Response->addHeader("Access-Control-Max-Age", "3600");
+                
+        $code = $request->URL[0];
+        $Bd = new Bd();
+        $url = $Bd->seleccionar("codigos", "id = $code", "url")->fetch()['url'];
+
+        if(!empty($url)){
+            $this->Response->Location = $url;
+            $this->Response->Body = array("code: " => $code, "url" => $url);
+            $this->Response->StatusCode = HTTPCodes::FOUND;
+
+            exit($this->Response->send());
+        }
+
+        throw new NotFoundException("El código no está registrado.", 1);
+    }
+}
+
+$index = new index();
+$index->init();
