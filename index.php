df3f8c074cd40faa96c00cdec83e89a740a26144
Enmanuel Basulto Martínez <enmanuel.basulto@uic.cu>
2022-02-14 17:19:01 -0500

    Arreglando problemas....

--- index.php
+++ index.php
@@ -60,10 +60,11 @@ final class index {
         $url = $Bd->seleccionar("codigos", "id = '$code'", "url")->fetch()['url'];
 
         if($url != null){
+            $this->Response->StatusCode = HTTPCodes::FOUND;
             $this->Response->Location = $url;
             $this->Response->Body = array("code: " => $code, "url" => $url);
-            $this->Response->StatusCode = HTTPCodes::FOUND;
 
+            print_r($this->Response);
             exit($this->Response->send());
         }
 
